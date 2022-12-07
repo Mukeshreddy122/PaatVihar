f0c372af786b99e68f30c65da44271e022c84803
Mukesh Reddy <mukeshvancha122@gmail.com>
2022-12-07 23:23:37 +0530

    Working on UI functionality and UI .With limited data

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,23 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css"
+import {BrowserRouter as Router ,Routes,Route} from "react-router-dom"
+import Header from "./component/header/Header";
+import Home from "./Pages/home/Home";
+import SchoolList from "./component/schoolList/SchoolList";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+        <Header/>
+        <Routes>
+          <Route path="/"  element={<Home/>}></Route>
+          <Route path="/Popular" element={<SchoolList/>}></Route>
+          <Route path="/Maps" element={<h1>Error Page</h1>}></Route>
+          <Route path="/Upcoming" element={<h1>Three</h1>}></Route>
+          <Route path="/Top" element={<h1>Three</h1>}></Route>
+        </Routes>
+
+      </Router> 
     </div>
   );
 }
---
+++ frontend/src/Pages/home/Home.js
@@ -0,0 +1,64 @@
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import "./Home.css";
+import "react-responsive-carousel/lib/styles/carousel.min.css";
+import { Carousel } from "react-responsive-carousel";
+import Logo from "../../component/header/Pv.png"
+import { Link } from "react-router-dom";
+const Home = () => {
+  const [schools, setSchools] = useState([]);
+  useEffect(() => {
+    axios
+      .get("/api/schools")
+      .then((res) => {
+        setSchools(res.data.school);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }, []);
+  return (
+    <>
+      <div className="poster">
+        <Carousel
+          showThumbs={false}
+          autoPlay={true}
+          transitionTime={3}
+          infiniteLoop={true}
+          showStatus={false}
+        >
+         
+            {
+                schools.map(school=>(
+                    <Link style={{textDecoration:"none",color:"white"}} to={`/movie/${school.id}`}>
+                        <div className="posterImage">
+                            <img src={Logo}/>
+                        </div>
+                        <div className="posterImage__overlay">
+                            <div className="posterImage__title">{school?school.name:""}</div>
+                            <div className="posterImage__runtime">
+                                {school?school.boardOfEduction:""}
+                                <span className="posterImage__fee">
+                                    {school?school.fee:""}
+                                </span>
+
+                            </div>
+                            <div className="posterImage__address">{
+                                school?school.address:""
+                            }</div>
+
+                        </div>
+
+
+
+                    </Link>
+                
+                )
+                )
+            }
+        </Carousel>
+      </div>
+    </>
+  );
+};
+export default Home;
